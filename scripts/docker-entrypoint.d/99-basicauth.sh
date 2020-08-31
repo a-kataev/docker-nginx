15b0f714a0edc6b227197b7ffea8159071ff8731
Alex Kataev <dlyavsehpisem@gmail.com>
2020-08-31 10:50:58 +0500

    update 99-basicauth.sh

--- scripts/docker-entrypoint.d/99-basicauth.sh
+++ scripts/docker-entrypoint.d/99-basicauth.sh
@@ -1,21 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-basicauth_filepath=${BASICAUTH_FILESPATH:-/etc/nginx/conf.d}
-basicauth_envprefix=${BASICAUTH_ENVPREFIX:-BASICAUTH}
+ME=$(basename $0)
 
-names="$(env | sort | grep -Eo '^'${basicauth_envprefix}'_([A-Z0-9]{1,})_' | \
-  sed 's/^'${basicauth_envprefix}'_\([A-Z0-9]*\)_/\1/g' | uniq)"
+NGINX_BASICAUTH_FILES_PATH=${NGINX_BASICAUTH_FILES_PATH:-/etc/nginx/conf.d}
+NGINX_BASICAUTH_ENV_PREFIX=${NGINX_BASICAUTH_ENV_PREFIX:-BASICAUTH}
+
+if [[ ! -w "${NGINX_BASICAUTH_FILES_PATH}" ]]; then
+  echo >&3 "$ME: error: ${NGINX_BASICAUTH_FILES_PATH} is not writable"
+  exit 0
+fi
+
+basicauth=$(env | sort | grep -Eo '^'${NGINX_BASICAUTH_ENV_PREFIX}'_([A-Z0-9]{1,})((_[0-9]{1,}_)|_)(USER|PASS)=.+$' || true)
+names="$(echo "${basicauth}" | sed 's/^'${NGINX_BASICAUTH_ENV_PREFIX}'_\([A-Z0-9]*\)_\(.*\)$/\1/g' | uniq)"
 for name in ${names}; do
-  prefixes="$(env | sort | grep -Eo '^'${basicauth_envprefix}'_'${name}'_([0-9]{1,})_(USER|PASS)=.+' | \
-    grep -Eo '^'${basicauth_envprefix}'_'${name}'_([0-9]{1,})_' | uniq)"
-  name="$(echo "${name}" | tr '[:upper:]' '[:lower:]').htpasswd"
+  prefixes="$(echo "${basicauth}" | grep -Eo '^'${NGINX_BASICAUTH_ENV_PREFIX}'_'${name}'((_[0-9]{1,}_)|_)' | uniq)"
+  htpasswd_file="${NGINX_BASICAUTH_FILES_PATH}/$(echo "${name}" | tr '[:upper:]' '[:lower:]').htpasswd"
   for prefix in ${prefixes}; do
     user="$(env | grep -Eo '^'${prefix}'USER=.+' | sed 's/^'${prefix}'USER=//g')"
-     pass="$(env | grep -Eo '^'${prefix}'PASS=.+' | sed 's/^'${prefix}'PASS=//g')"
+    pass="$(env | grep -Eo '^'${prefix}'PASS=.+' | sed 's/^'${prefix}'PASS=//g')"
     if [[ -n "${user}" ]] && [[ -n "${pass}" ]]; then
-      echo "$(htpasswd -bn "${user}" "${pass}")" >> "${basicauth_filepath}/${name}"
+      echo "$(htpasswd -bn "${user}" "${pass}")" >> "${htpasswd_file}"
+      echo >&3 "$ME: Adding a user '${user}' on ${htpasswd_file}"
     fi
   done
 done
